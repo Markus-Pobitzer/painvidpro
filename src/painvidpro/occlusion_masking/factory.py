3fae8d00e84308a5e5b4384f3068a9b9a1c308e8
Markus Pobitzer <MarkusPobitzer@sinaia>
2025-01-20 16:26:14 +0000

    occlusion_masking: add occlusion masking algorithms

---
+++ src/painvidpro/occlusion_masking/factory.py
@@ -0,0 +1,38 @@
+"""Factory for the Occlusion Masking."""
+
+from typing import Any, Dict
+
+from painvidpro.occlusion_masking.occlusion_masking_accumulate_diff import OcclusionMaskingAccumulateDiff
+from painvidpro.occlusion_masking.occlusion_masking_base import OcclusionMaskingBase
+from painvidpro.occlusion_masking.occlusion_masking_frame_diff import OcclusionMaskingFrameDiff
+
+
+class OcclusionMaskingFactory:
+    @staticmethod
+    def build(algorithm: str, params: Dict[str, Any]) -> OcclusionMaskingBase:
+        """Builds an OcclusionMasking Algorithm.
+
+        Args:
+            algorithm: The Algorithm as a string.
+            params: The parameters as a dict.
+
+        Returns:
+            An OcclusionMasking Algorithm.
+
+        Raises:
+            ValueError if the alogrithm is unknown or the algorithm can not
+            be build.
+        """
+        instance: OcclusionMaskingBase
+        if algorithm == "OcclusionMaskingFrameDiff":
+            instance = OcclusionMaskingFrameDiff()
+        elif algorithm == "OcclusionMaskingAccumulateDiff":
+            instance = OcclusionMaskingAccumulateDiff()
+        else:
+            raise ValueError(f"Unknown algorithm: {algorithm}")
+
+        success, error_message = instance.set_parameters(params)
+        if not success:
+            raise ValueError(f"Error setting parameters: {error_message}")
+
+        return instance
