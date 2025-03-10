d075a16e07079e40cba5ebe3e52d7a266b1e8600
Markus Pobitzer <markus@Desktop.>
2025-03-10 08:46:26 +0100

    scritps: yoda_slurm fix SBATCH command

--- scripts/process_video/yoda_slurm.sh
+++ scripts/process_video/yoda_slurm.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 #SBATCH -A staff
 #SBATCH -J process_video
-#SBATCH -t 06:00:00              # Max runtime 6 hours
+#SBATCH -t 03:00:00              # Max runtime 3 hours
 #SBATCH --gres=gpu:1             # Request 1 GPU
 #SBATCH -N 1                     # Use 1 node
 #SBATCH --cpus-per-task=4        # 4 CPU cores per task
 #SBATCH --mem=8G                 # 8GB memory
+#SBATCH -o slurm_log/slurm-%j.out
+#SBATCH -e slurm_log/slurm-%j.err
 
 # Validate input parameters
 if [ $# -ne 3 ]; then
@@ -16,15 +18,6 @@ fi
 BASE_DIR="$1"
 SOURCE="$2"
 VIDEO_ID="$3"
-
-# Sanitize BASE_DIR and SOURCE for use in filenames
-SANITIZED_BASE_DIR=$(echo "$BASE_DIR" | tr '/' '_' | tr -cd '[:alnum:]._-')
-SANITIZED_SOURCE=$(echo "$SOURCE" | tr '/' '_' | tr -cd '[:alnum:]._-')
-
-# Set output and error log files with sanitized names
-#SBATCH -o slurm_log/slurm-%j_${SANITIZED_BASE_DIR}_${SANITIZED_SOURCE}.out
-#SBATCH -e slurm_log/slurm-%j_${SANITIZED_BASE_DIR}_${SANITIZED_SOURCE}.err
-
 
 # Activate poetry if not already
 eval $(poetry env activate)
