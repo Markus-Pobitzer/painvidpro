53e736984441b61e830d7f832d73244cd0d3f3df
Markus Pobitzer <17593719+Markus-Pobitzer@users.noreply.github.com>
2025-12-21 17:52:08 +0100

    src: cli export_pipeline

--- src/painvidpro/cli/export_pipeline.py
+++ src/painvidpro/cli/export_pipeline.py
@@ -1,10 +1,10 @@
-"""Creates a directory structure with pickeled frames."""
+"""Creates a directory structure with test and train split."""
 
 import argparse
 import os
 import sys
 
-from painvidpro.export_dataset.export_video_pkl import export_video_to_pkl
+from painvidpro.utils.export_dataset import export_pipeline
 
 
 def parse_arguments() -> argparse.Namespace:
@@ -22,12 +22,6 @@ def parse_arguments() -> argparse.Namespace:
     parser.add_argument("pipeline_path", type=str, help="Directory containing the pipeline.")
     parser.add_argument("output_dir", type=str, help="Output directory for processed Hugging Face dataset")
     parser.add_argument("--train_split_size", type=float, default=0.9, help="The training split size, between [0, 1].")
-    parser.add_argument(
-        "--max_num_frames",
-        type=int,
-        default=-1,
-        help=("If set to a vlue greater than 0, takes at most max_num_frames."),
-    )
     parser.add_argument(
         "--seed",
         type=int,
@@ -59,12 +53,11 @@ def main() -> None:
         validate_arguments(args)
 
         # Step 1: Create Hugging Face DatasetDict
-        print("\n🚀 Creating Pickel dataset ...")
-        export_video_to_pkl(
+        print("\n🚀 Creating dataset ...")
+        export_pipeline(
             pipeline_path=args.pipeline_path,
             output_dir=args.output_dir,
             train_split_size=args.train_split_size,
-            max_num_frames=args.max_num_frames,
             seed=args.seed,
         )
         print(f"✅ Dataset saved to {args.output_dir}")
---
+++ src/painvidpro/utils/export_dataset.py
@@ -0,0 +1,87 @@
+"""Exports, i.e. mainly copies the h5 file in a shared dataset."""
+
+import json
+import logging
+import random
+import shutil
+from os.path import join
+from typing import Any, Dict, List, Optional
+
+from painvidpro.data_storage.hdf5_video_archive import DynamicVideoArchive
+from painvidpro.pipeline.pipeline import Pipeline
+
+
+logger = logging.getLogger(__name__)
+
+
+def export_pipeline(
+    pipeline_path: str,
+    output_dir: str,
+    train_split_size: float = 0.9,
+    video_source_split: Optional[str] = "video_sources/video_sources_split.json",
+    entries_in_video_source_split: bool = False,
+    seed: Optional[int] = 42,
+    frame_data_file="frame_data.h5",
+) -> None:
+    """Processes video pipeline data and copies it into a split directory structure (train/test).
+
+    Args:
+        pipeline_path: Directory where the source pipeline is stored.
+        output_dir: Target directory to store the exported video files.
+        train_split_size: Float between 0 and 1 representing the proportion of training data.
+        video_source_split: Path to a JSON file defining specific splits.
+        entries_in_video_source_split: If True, only videos found in the split file are exported.
+        seed: Random seed for reproducible splitting.
+        frame_data_file: The name of the HDF5 file containing frame metadata.
+    """
+    pipe = Pipeline(base_dir=pipeline_path)
+    video_dir_list: List[str] = []
+
+    if seed is not None:
+        random.seed(seed)
+
+    split_dict: Dict[str, Any] = {}
+    if video_source_split is not None:
+        # Check if entry is in file
+        with open(video_source_split) as f:
+            split_dict = json.load(f)
+
+    # Process videos and collect metadata
+    for source in pipe.video_item_dict:
+        for video_id, _ in pipe.video_item_dict[source].items():
+            video_dir = join(pipeline_path, source, video_id)
+            with DynamicVideoArchive(join(video_dir, frame_data_file)) as frame_dataset:
+                metadata = frame_dataset.get_global_metadata()
+                if metadata.get("exclude_video", False):
+                    logger.info((f"Excluding {video_dir}, since the exclude_video is set in the metadata."))
+                    continue
+                if len(frame_dataset) == 0:
+                    logger.info((f"Excluding {video_dir}, since no extracted frames were found."))
+                    continue
+
+            if split_dict and source in split_dict and video_id in split_dict[source]:
+                split = split_dict[source][video_id]
+            else:
+                if entries_in_video_source_split:
+                    # Entry was ot found
+                    logger.info(
+                        (
+                            f"Excluding {video_dir}, it was not found in the video_source_split {video_source_split} and entries_in_video_source_split was set."
+                        )
+                    )
+                    continue
+
+                split = "test"
+                if random.random() <= train_split_size:
+                    split = "train"
+
+            out_dir = join(output_dir, split, source, video_id)
+            try:
+                shutil.copytree(video_dir, out_dir)
+            except Exception as e:
+                logger.info((f"Was not able to copy {video_dir} to {out_dir}, skipping entry due to error: {e}"))
+                continue
+
+            video_dir_list.append(video_dir)
+
+    logger.info(f"Exported {len(video_dir_list)} videos to {output_dir}.")
