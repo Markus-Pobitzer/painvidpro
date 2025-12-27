e491c4be92d62fc0c61a61ae12bbc710a16bd167
Markus Pobitzer <17593719+Markus-Pobitzer@users.noreply.github.com>
2025-12-27 18:38:01 +0100

    utils: use h5 video archive

--- src/painvidpro/utils/ref_frame_tags.py
+++ src/painvidpro/utils/ref_frame_tags.py
@@ -1,24 +1,26 @@
 from pathlib import Path
 from typing import List
 
-from painvidpro.utils.metadata import load_metadata, save_metadata
+from painvidpro.data_storage.hdf5_video_archive import DynamicVideoArchive
 
 
-def clean_ref_frame_tags(video_dir_list: List[str], tags_dir="reference_frame_tags"):
+def clean_ref_frame_tags(video_dir_list: List[str], tags_dir="reference_frame_tags", frame_data_file="frame_data.h5"):
     """
     Removes the 'reference_frame_tags' entry from the metadata and deletes the corresponding tags directory for each video directory in the provided list.
 
     Args:
         video_dir_list (List[str]): List of paths to video directories.
         tags_dir (str, optional): Name of the directory containing reference frame tags to be removed. Defaults to "reference_frame_tags".
+        tags_dir (str): Name of the DynamicVideoArchive.
 
     Side Effects:
         - Modifies the metadata by removing the 'reference_frame_tags' key if present.
         - Deletes the tags directory within each video directory.
     """
     for video_dir in video_dir_list:
-        vd = Path(video_dir)
-        succ, metadata = load_metadata(vd)
-        if "reference_frame_tags" in metadata:
-            metadata.pop("reference_frame_tags")
-            save_metadata(video_dir=vd, metadata=metadata)
+        frame_data_path = Path(video_dir) / frame_data_file
+        with DynamicVideoArchive(filename=str(frame_data_path)) as archive:
+            metadata = archive.get_global_metadata()
+            if tags_dir in metadata:
+                metadata.pop(tags_dir)
+                archive.set_global_metadata(tags_dir, [])
